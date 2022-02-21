83750ab38ee9f437185e71cae3d8ad303846bbcd
nicolas <nicolas@archipels.io>
2022-02-21 12:12:28 +0100

    fix: fix player object

--- player.py
+++ player.py
@@ -1,4 +1,20 @@
 class Player:
     def __init__(self):
-        name = ""
-        score = 0
+        self.name = ""
+        self.score = 0
+    
+    @property
+    def score(self):
+        return self._score
+    
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, name):
+        self._name = name
+    
+    @name.setter
+    def score(self, score):
+        self._score = score
